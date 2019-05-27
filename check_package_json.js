a7410d3179987aaed8c2b5f354896de4bb161a72
Bryan Larsen <bryan@larsen.st>
2019-05-27 15:24:23 -0400

    package.json check support for modules

--- check_package_json.js
+++ check_package_json.js
@@ -22,35 +22,50 @@ function main() {
     }
   }
 
-  let yarndeps = new Set();
-  for (const dep of Object.keys(yarnlock.object)) {
-    let m = dep.match(/^@threekit\/(.*)\@(.*)/);
-    if (m) {
-      if (yarndeps.has(m[1])) {
-        console.log(`@threekit/${m[1]} included multiple times in yarn.lock`);
-        failures += 1;
-      } else {
-        yarndeps.add(m[1]);
+  if (process.argv[2] === 'module') {
+    try {
+      child_process.execSync('git ls-files --error-unmatch yarn.lock');
+      failures += 1;
+      console.log("Don't check in yarn.lock for modules");
+    } catch (e) {
+      true;
+    }
+    return failures;
+  } else if (process.argv[2] === 'service') {
+    let yarndeps = new Set();
+    for (const dep of Object.keys(yarnlock.object)) {
+      let m = dep.match(/^@threekit\/(.*)\@(.*)/);
+      if (m) {
+        if (yarndeps.has(m[1])) {
+          console.log(`@threekit/${m[1]} included multiple times in yarn.lock`);
+          failures += 1;
+        } else {
+          yarndeps.add(m[1]);
+        }
       }
     }
-  }
 
-  for (const dir of fs.readdirSync('./node_modules/@threekit')) {
-    let pkg2 = JSON.parse(
-      fs.readFileSync(`./node_modules/@threekit/${dir}/package.json`, 'utf8')
-    );
+    for (const dir of fs.readdirSync('./node_modules/@threekit')) {
+      let pkg2 = JSON.parse(
+        fs.readFileSync(`./node_modules/@threekit/${dir}/package.json`, 'utf8')
+      );
 
-    for (const dep of Object.keys(pkg2.dependencies)) {
-      if (deps[dep]) {
-        console.log(
-          `${dep} is a dependency in both our package.json and @threekit/${dir}'s.   Please move it into peerDependencies in @threekit/${dir}`
-        );
-        failures += 1;
+      for (const dep of Object.keys(pkg2.dependencies)) {
+        if (deps[dep]) {
+          console.log(
+            `${dep} is a dependency in both our package.json and @threekit/${dir}'s.   Please move it into peerDependencies in @threekit/${dir}`
+          );
+          failures += 1;
+        }
       }
     }
-  }
 
-  return failures;
+    return failures;
+  } else {
+    failures += 1;
+    console.log('Usage: check_package_json (service|module)');
+    return failures;
+  }
 }
 
 process.exit(main());
